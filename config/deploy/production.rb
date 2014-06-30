ff8dd08858b356ea1b0ab3bc8992ecfb2885dc0e
Stefflan00 <stefan.maier09@gmail.com>
2014-06-30 17:01:39 +0200

    change db settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '54.76.205.157', user: 'deploy', roles: %w{web app}
+set :password, ask('Password', nil)
+server '54.76.205.157', port: 22, user: 'deploy', password: fetch(:password), roles: %w{web app}
