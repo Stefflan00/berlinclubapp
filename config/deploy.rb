91417ae4cd824554ac1f7db172e637f217909408
Stefflan00 <stefan.maier09@gmail.com>
2014-06-30 16:39:44 +0200

    change some user and deploy details

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,7 @@
 set :application, 'BerlinClubApp'
 set :repo_url, 'git@github.com:Stefflan00/berlinclubapp.git'
 set :scm, :git
-set :deploy_to, '/home/ubuntu/bitterblueberry'
-#default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-ssh_options[:auth_methods] = ["publickey"]
-ssh_options[:keys] = ["/Users/stefanmaier/Documents/Programming/Hosting/Amazon/3rd_amazon_machine.pem"]
+set :deploy_to, '/home/deploy/berlinclubapp'
 #set :linked_files, %w{config/database.yml}
 #set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
