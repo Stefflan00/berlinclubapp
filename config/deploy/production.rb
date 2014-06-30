7e0a2c642d9ea3c6d91f7ccea602ceabd385bddc
Stefflan00 <stefan.maier09@gmail.com>
2014-06-30 15:39:34 +0200

    change ssh options

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '54.76.205.157', user: 'ubuntu', roles: %w{web app}
-ssh_options[:keys] = ["#{ENV['HOME']}/Users/stefanmaier/Documents/Programming/Hosting/Amazon/3rd_amazon_machine.pem"]
+server '54.76.205.157', user: 'ubuntu', roles: %w{web app}, ssh_options[:keys] = ["#{ENV['HOME']}/Users/stefanmaier/Documents/Programming/Hosting/Amazon/3rd_amazon_machine.pem"]
